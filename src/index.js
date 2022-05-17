f9e55d4b3c81239a683ad29aaf7225a3a1803600
Alfonso Jimenez <ajimenez@agros.tech>
2022-05-16 23:11:50 -0500

    feat: Initial configuration

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/index.js';
+import config from './config/index.js';
+import ErrorMiddleware from './core/errors/errorConverterMiddleware.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../docs/openapi.json';
+import db from './core/db/connection.js';
+
+const app = express();
+const basePath = '/voting';
+
+app.use(cors());
+
+app.set('env', config.env);
+
+if (config.env === 'development') {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
+
+app.disable('x-powered-by');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res, next) => {
+  return res.status(200).json({
+    status: 'ok',
+    name: 'ms-voting',
+    version: config.API_VERSION
+  });
+});
+
+app.use(basePath, routes);
+app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
+
+app.use(ErrorMiddleware);
+
+db.authenticate()
+  .then(async () => {  
+    app.listen(config.port, () => {
+      console.warn(`Server started on http://localhost:${config.port}${basePath}  ()`);
+    });
+  })
+  .catch((err) => {
+    console.warn(`Error:${err}`);
+  });
+
+export default app;
